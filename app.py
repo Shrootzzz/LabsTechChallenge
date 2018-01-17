d143521a2a17c35f8d957b2ca5e99ea6f2672e92
William Wang <William@dyn-160-39-139-132.dyn.columbia.edu>
2018-01-16 22:32:32 -0500

    update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
 
+@app.route('/ww2472')
+def me():
+	return render_template('ww2472.html')
 
 if __name__ == '__main__':
     app.run()
